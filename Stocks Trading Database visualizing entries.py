61068a0020e086c1e348413b2784b10fb1932c1f
cindymenp <52049218+cindymenp@users.noreply.github.com>
2022-02-20 12:49:17 +0100

    Update Stocks Trading Database visualizing entries.py

--- Stocks Trading Database visualizing entries.py
+++ Stocks Trading Database visualizing entries.py
@@ -7,7 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 from IPython.display import display, Image
-
+import numpy as np
 
 # get data file names
 path =r'/Users/cindymendoncapaez/opt/anaconda3/lib/python3.8/site-packages/folium/finance project/Breakout US stocks' 
@@ -22,13 +22,43 @@ big_frame = pd.concat(dfs, ignore_index=True)
 
 #set the column names
 big_frame.columns = ['time', 'open', 'high', 'low', 'close', 'MA50', 'MA20', 'MA10', 'ADR', 'DV M', 'MA20 DV M', 'entry/exit']
+big_frame
+
+
+#file=os.listdir(csv_directory)):
+#tickers = pd.read_csv(path+file, sep = ',')
+combined_data = big_frame
+#combined_data.columns = ['time', 'open', 'high', 'low', 'close', 'MA50', 'MA20', 'MA10', 'ADR', 'DV M', 'MA20 DV M', 'entry/exit'] 
+#days_high = tickers["high"]
+#days_low = tickers["low"]
+#days_close = tickers["close"]
+combined_data = combined_data[combined_data['entry/exit'].isin(["entry","exit"])]
+combined_data['entry_price']= np.NaN
+combined_data['exit_price'] = np.NaN    
+combined_data['profit'] = np.NaN
+combined_data['risk'] = np.NaN
+combined_data['R'] = np.NaN
+combined_data.loc[combined_data['entry/exit'].isin(["entry"]), 'entry_price'] = (combined_data['low'] + combined_data['high']) /2
+combined_data.loc[combined_data['entry/exit'].isin(["exit"]), 'exit_price'] = combined_data['close']
+combined_data['entry_price'] = combined_data['entry_price'].fillna(method='ffill')
+combined_data.loc[combined_data['entry/exit'].isin(["exit"]), 'profit'] = combined_data['exit_price']-combined_data['entry_price']
+combined_data.loc[combined_data['entry/exit'].isin(["entry"]),'risk'] = combined_data['entry_price']-combined_data['low']
+combined_data['risk'] = combined_data['risk'].fillna(method='ffill')
+combined_data.loc[combined_data['entry/exit'].isin(["exit"]), 'R'] = combined_data['profit']/combined_data['risk']
+    
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import cufflinks as cf
+cf.go_offline()
+    
+combined_data.drop(combined_data[combined_data['entry/exit'] == 'entry'].index, inplace=True)
+combined_data.iplot(kind='scatter',x='ADR',y='R',mode='markers',size=10)
 
 #find the entries
 
 big_frame = big_frame.loc[big_frame['entry/exit']=='entry']
 big_frame = big_frame.replace('entry',1)
-
-#bar graph done with PlotLy
+big_frame
 
 df = big_frame
 
@@ -41,18 +71,17 @@ fig.add_trace(go.Bar(
     xperiodalignment="middle"
 ))
 fig.update_xaxes(showgrid=True, ticklabelmode="period")
+fig.show()
 
-
-#Ticker + Graph finder
-
-csv_directory = '/Users/cindymendoncapaez/Downloads/Breakout US stocks/breakout/csv/'
+csv_directory = 'C:/Users/baszo/Desktop/Breakout US stocks/A/'
 
 @interact
 def show_csv(file=os.listdir(csv_directory)):
-    display(pd.read_csv(csv_directory+file, sep = ';'))
+    global code
+    display(pd.read_csv(csv_directory+file, sep = ','))
     code = file.split('.csv')[0]
 
-fdir = '/Users/cindymendoncapaez/Downloads/Breakout US stocks/breakout/graphs/'
+fdir = '/Users/cindy/Desktop/Breakout US stocks/A/images/'
 
 @interact
 def show_images(ticker_name = code):
@@ -60,103 +89,21 @@ def show_images(ticker_name = code):
     list_match = [x for x in list_code if x.startswith(ticker_name + '_')]
     for file in list_match:
         display(Image(fdir+file))
- 
-#Plotly line graph
-
-path = '/Users/cindymendoncapaez/Downloads/Breakout US stocks/breakout/csv/'
-
-global code
+        
+        
+ path = '/Users/cindy/Desktop/Breakout US stocks/A/'
 
 @interact
 def show_csv(file=os.listdir(path)):
     global tickers
-    tickers = pd.read_csv(path+file, sep = ';')
+    tickers = pd.read_csv(path+file, sep = ',')
     code = file.split('.csv')[0]
 
     
 @interact
 def display_time_series(param = tickers):
     list_code = os.listdir(path)
-    list_match = [x for x in list_code if x.startswith(param + '_')]
     fig = px.line(tickers, x=tickers['time'], y=param) 
     fig.show()
 
 
-#Values
-
-days_high = tickers["high"]
-days_low = tickers["low"]
-days_close = tickers["close"]
-
-daily_ranges = []
-
-for ticker in tickers:
-
-    profit = (days_low + days_high) /2
-   
-combined_data = tickers
-combined_data['entry_price'] =  entry_price
-combined_data['exit_price'] = exit_price
-
-
-#First entry & Last exit code with profit. Please notice that the underlined rows are the ones to look at.
-
-csv_directory = '/Users/cindymendoncapaez/Downloads/Breakout US stocks/breakout/csv/'
-
-@interact
-def show_csv(file=os.listdir(csv_directory)):
-    tickers = pd.read_csv(path+file, sep = ',')
-    combined_data = tickers
-    combined_data.columns = ['time', 'open', 'high', 'low', 'close', 'MA50', 'MA20', 'MA10', 'ADR', 'DV M', 'MA20 DV M', 'entry/exit'] 
-    days_high = tickers["high"]
-    days_low = tickers["low"]
-    days_close = tickers["close"]
-    combined_data['entry_price'] = (days_low + days_high) /2
-    combined_data['exit_price'] = days_close
-    last_exit = combined_data.groupby(['entry/exit'], as_index='exit').last()
-    first_entry = combined_data.groupby(['entry/exit'], as_index='entry').first()
-    last_exit_style = last_exit.style.set_properties(subset = pd.IndexSlice[['exit'], :], **{'background-color' : 'yellow'})
-    first_entry_style = first_entry.style.set_properties(subset = pd.IndexSlice[['entry'], :], **{'background-color' : 'yellow'})
-    display (first_entry_style)
-    display (last_exit_style)
-
-    
-    
-#Profit, risk and R calculators
-
-@interact
-def show_csv(file=os.listdir(csv_directory)):
-    tickers = pd.read_csv(path+file, sep = ',')
-    combined_data = tickers
-    combined_data.columns = ['time', 'open', 'high', 'low', 'close', 'MA50', 'MA20', 'MA10', 'ADR', 'DV M', 'MA20 DV M', 'entry/exit'] 
-    days_high = tickers["high"]
-    days_low = tickers["low"]
-    days_close = tickers["close"]
-    combined_data = combined_data[combined_data['entry/exit'].isin(["entry","exit"])]
-    combined_data['entry_price']= np.NaN
-    combined_data['exit_price'] = np.NaN    
-    combined_data['profit'] = np.NaN
-    combined_data['risk'] = np.NaN
-    combined_data['R'] = np.NaN
-    combined_data.loc[combined_data['entry/exit'].isin(["entry"]), 'entry_price'] = (combined_data['low'] + combined_data['high']) /2
-    combined_data.loc[combined_data['entry/exit'].isin(["exit"]), 'exit_price'] = combined_data['close']
-    combined_data['entry_price'] = combined_data['entry_price'].fillna(method='ffill')
-    combined_data.loc[combined_data['entry/exit'].isin(["exit"]), 'profit'] = combined_data['exit_price']-combined_data['entry_price']
-    combined_data.loc[combined_data['entry/exit'].isin(["entry"]),'risk'] = combined_data['entry_price']-combined_data['low']
-    combined_data['risk'] = combined_data['risk'].fillna(method='ffill')
-    combined_data.loc[combined_data['entry/exit'].isin(["exit"]), 'R'] = combined_data['profit']/combined_data['risk']
-    display(combined_data)
-    
-#Returns Calculator
-
-returns = combined_data
-returns['Return'] = returns['close'].pct_change()
-returns.head()
-
-
-#Returns graph
-
-sns.distplot(returns['Return'],color='green',bins=200)
-    
-print(big_frame)
-print(combined_data)
